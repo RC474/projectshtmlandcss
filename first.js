47d29603b56b22737bf804ccd2b5b9e45f86ab0b
Ramesh Chuwan <rameshchuwan19@gmail.com>
2019-09-07 18:58:19 -0500

    initial commit

---
+++ first.js
@@ -0,0 +1,11 @@
+var firstName = prompt("Enter your first name:");
+var lastName = prompt("Enter your last name:");
+var age = prompt("Enter your age:");
+
+var userFullName = "Your first name is " + firstName + " and your last name is " + lastName + ".";
+
+console.log(userFullName);
+
+var userAge = "You are " + age + " years old."
+
+console.log(userAge);
