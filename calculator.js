82c122b2c35141e5b7b930c0b474a9a511a1fbc3
Ramesh Chuwan <rameshchuwan19@gmail.com>
2019-09-07 19:33:23 -0500

    bugs fixed

---
+++ calculator.js
@@ -0,0 +1,3 @@
+var age = prompt("Enter your age:");
+var daysLived = "You lived close to " + age * 365.25 + " days."; //.25 is for leap year, which is once in every four years.
+console.log(daysLived);
